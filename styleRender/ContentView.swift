cd3d22fb8413fadf9a31a9750c410388c3a7ce35
胡凯凡 <975293830@qq.com>
2026-04-26 18:14:18 +0800

    Initial Commit

---
+++ styleRender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  styleRender
+//
+//  Created by 胡凯凡 on 2026/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ styleRender/styleRenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  styleRenderApp.swift
+//  styleRender
+//
+//  Created by 胡凯凡 on 2026/4/26.
+//
+
+import SwiftUI
+
+@main
+struct styleRenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
